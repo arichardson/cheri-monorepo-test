qwattash/lld

Make external-capsizefix with shlibs an error as it will generate incorrect values

ELF/Relocations.cpp
@@ -564,8 +564,12 @@ static RelExpr adjustExpr(SymbolBody &Body, RelExpr Expr, RelType Type,
 
   // HACK: clang emits a read-only __cap_relocs, but for PIC code we need to
   //       emit dynamic relocations for its contents (REL32/64/NONE).
-  if (Config->EMachine == EM_MIPS && Config->Pic && S.Name == "__cap_relocs")
+  if (Config->EMachine == EM_MIPS && Config->Pic && S.Name == "__cap_relocs") {
+    if (!Config->ProcessCapRelocs)
+      error("capsizefix will not work with dynamic libraries, please remove "
+            "-no-process-cap-relocs from the linker invocation!");
     return Expr;
+  }
 
   // If we got here we know that this relocation would require the dynamic
   // linker to write a value to read only memory.

arichardson/cheri-monorepo-test

lld/ELF/Relocations.cpp
@@ -771,8 +771,12 @@ static RelExpr processRelocAux(InputSectionBase &Sec, RelExpr Expr,
   bool CanWrite = (Sec.Flags & SHF_WRITE) || !Config->ZText;
   // HACK: clang emits a read-only __cap_relocs, but for PIC code we need to
   //       emit dynamic relocations for its contents (REL32/64/NONE).
-  if (Config->EMachine == EM_MIPS && Config->Pic && Sec.Name == "__cap_relocs")
-    CanWrite = true;
+  if (Config->EMachine == EM_MIPS && Config->Pic && Sec.Name == "__cap_relocs") {
+    if (!Config->ProcessCapRelocs)
+      error("capsizefix will not work with dynamic libraries, please remove "
+              "-no-process-cap-relocs from the linker invocation!");
+    return Expr;
+  }
 
   if (CanWrite) {
     // R_GOT refers to a position in the got, even if the symbol is preemptible.